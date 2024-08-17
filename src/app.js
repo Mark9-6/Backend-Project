dcc64b8ccf325938afeff4698159a0504078cc5b
Swastik <98447347+Mark9-6@users.noreply.github.com>
2024-08-17 19:05:39 +0530

    setup done

--- src/app.js
+++ src/app.js
@@ -16,5 +16,12 @@ app.use(express.static("public")) //
 app.use(cookieParser())
 
 
+//rputes 
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users" , userRouter);
+
+
+
 export {app}
 

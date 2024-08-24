9e77cd27263da2783486f96e6fa098e622937b90
Swastik <98447347+Mark9-6@users.noreply.github.com>
2024-08-24 19:47:35 +0530

    use Await in db operations

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -248,7 +248,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
         throw new ApiError(400 , "All fields are required")
       }
       
-       const user = User.findByIdAndUpdate(req.user?._id , 
+       const user = await User.findByIdAndUpdate(req.user?._id , 
         {
           $set : {
             fullName:fullName,

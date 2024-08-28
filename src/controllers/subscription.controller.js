8f1576b3db20ed658ddd59d6bb56e155516a7e1c
Swastik <98447347+Mark9-6@users.noreply.github.com>
2024-08-29 01:21:48 +0530

    corrected previus commit

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -58,6 +58,6 @@ const getSubscribedChannels = asyncHandler(async(req,res)=>{
 
 export {
     toggleSubscription,
-    getSubscribedChannels,
+    getUserChannelSubscribers,
     getSubscribedChannels
 }
